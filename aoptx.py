3997b2e3f55c488b98d2dc8eb59eb2f602b803e3
Graham Markall <gmarkall@nvidia.com>
2021-12-01 22:23:45 +0000

    Add descriptive comments to harness

--- aoptx.py
+++ aoptx.py
@@ -11,29 +11,55 @@ import sys
 
 
 def run(day):
+    # Disable low occupancy warnings - because we run only with a single block
+    # / thread (because we're not really interested in high performance for
+    # these solutions, but convenience for launching a handcoded PTX) Numba
+    # will warn us. Setting this elides that warning.
     config.CUDA_LOW_OCCUPANCY_WARNINGS = 0
+
+    # Determine paths of solutions and input for the current day
     base_dir = os.path.dirname(os.path.abspath(__file__))
     day_dir = os.path.join(base_dir, f'day{day}')
     part1_path = os.path.join(day_dir, 'part1.ptx')
     part2_path = os.path.join(day_dir, 'part2.ptx')
     input_path = os.path.join(day_dir, 'input')
 
+    # Read in today's input
     with open(input_path, 'r') as f:
         values = np.asarray([int(line.strip()) for line in f.readlines()])
 
+    # Move the data to the GPU and get a pointer to the data and the number of
+    # entries
     device_values = cuda.to_device(values)
     data_ptr = device_values.__cuda_array_interface__['data'][0]
     data_len = device_values.__cuda_array_interface__['shape'][0]
 
+    # "Forward declaration" of device functions in the solution PTX - these are
+    # needed so that Numba knows how to resolve calls to these functions. The
+    # declarations here specify functions that accept a pair of uint64 values
+    # (the pointer to the data, and the number of items), and return nothing.
     part1 = cuda.declare_device('part1', 'void(uint64, uint64)')
     part2 = cuda.declare_device('part2', 'void(uint64, uint64)')
 
+    # Kernel to call the solutions. It accepts the data pointer and length, and
+    # passes them to the solution functions. It does nothing else because the
+    # solutions are expected to print their result.
+    #
+    # The link kwarg is used to instruct Numba to link the PTX for part 1 and
+    # part 2 when linking the kernel.
     @cuda.jit('void(uint64, uint64)', link=[part1_path, part2_path])
     def wrapper(data_ptr, data_len):
         part1(data_ptr, data_len)
         part2(data_ptr, data_len)
 
+    # Launch the kernel with a single thread, because we only expect solutions
+    # to need a single thread at this point (perhaps this will change for
+    # future solutions).
     wrapper[1, 1](data_ptr, data_len)
+
+    # Synchronize to prevent the program exiting before printed solutions get
+    # flushed - printing inside kernels can occur asynchronously WRT to the
+    # main thread.
     cuda.synchronize()
 
 
