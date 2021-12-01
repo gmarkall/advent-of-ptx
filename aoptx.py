9d3923a9fb84ff6e22f752b85f971ba1b9ace1d8
Graham Markall <gmarkall@nvidia.com>
2021-12-01 10:04:19 +0000

    Day 1: Load in data and print first element

--- aoptx.py
+++ aoptx.py
@@ -2,32 +2,49 @@
 #
 # PTX execution harness using Numba
 #
-# Usage: aoptx.py <Solution PTX>
-#
-# The PTX file must contain a device function called `solution` that accepts no
-# arguments and prints out the solution.
+# Usage: aoptx.py <day>
 
 from numba import cuda, config
+import numpy as np
 import os
 import sys
 
 
-def run(filename):
+def run(day):
     config.CUDA_LOW_OCCUPANCY_WARNINGS = 0
-    solution = cuda.declare_device('solution', 'void()')
-    path = os.path.abspath(filename)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    day_dir = os.path.join(base_dir, f'day{day}')
+    ptx_path = os.path.join(day_dir, 'solution.ptx')
+    input_path = os.path.join(day_dir, 'input')
 
-    @cuda.jit('void()', link=[path])
-    def wrapper():
-        solution()
+    with open(input_path, 'r') as f:
+        values = np.asarray([int(line.strip()) for line in f.readlines()])
 
-    wrapper[1, 1]()
+    device_values = cuda.to_device(values)
+    data_ptr = device_values.__cuda_array_interface__['data'][0]
+
+    solution = cuda.declare_device('solution', 'void(uint64)')
+
+    @cuda.jit('void(uint64)', link=[ptx_path])
+    def wrapper(ptr):
+        solution(ptr)
+
+    wrapper[1, 1](data_ptr)
     cuda.synchronize()
+
+
+def usage():
+    print("Usage: aoptx.py <day>")
+    sys.exit(1)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: aoptx.py <Solution PTX>")
-        sys.exit(1)
+        usage()
 
-    run(sys.argv[1])
+    try:
+        day = int(sys.argv[1])
+    except ValueError:
+        usage()
+
+    run(day)
